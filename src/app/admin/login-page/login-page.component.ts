6adbd4d28e1a2db8e0f6e6295e58d5229016003a
Mariia Popleteeva <m.popleteeva@gmail.com>
2020-12-13 13:40:04 +0300

    login guard

--- src/app/admin/login-page/login-page.component.ts
+++ src/app/admin/login-page/login-page.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {FormControl, FormGroup, Validators} from '@angular/forms';
 import {User} from '../shared/interfaces';
 import {AuthService} from '../shared/services/auth.service';
-import {Router} from '@angular/router';
+import {ActivatedRoute, Params, Router} from '@angular/router';
 
 @Component({
   selector: 'app-login-page',
@@ -13,12 +13,20 @@ export class LoginPageComponent implements OnInit {
 
   form: FormGroup;
   submitted = false;
+  message: string;
 
   constructor(
     public auth: AuthService,
-    private route: Router) {}
+    private router: Router,
+    private route: ActivatedRoute) {}
 
   ngOnInit(): void {
+    this.route.queryParams.subscribe((params: Params) => {
+      if (params.loginAgain) {
+        this.message = 'Пожалуйста, введите данные';
+      }
+    });
+
     this.form = new FormGroup({
       email: new FormControl(null, [ Validators.required, Validators.email]),
       password: new FormControl(null, [Validators.required, Validators.minLength(6)]),
@@ -39,7 +47,7 @@ export class LoginPageComponent implements OnInit {
 
     this.auth.login(user).subscribe( () => {
       this.form.reset();
-      this.route.navigate(['/admin', 'dashboard']);
+      this.router.navigate(['/admin', 'dashboard']);
       this.submitted = false;
     }, error => {
       this.submitted = false;
